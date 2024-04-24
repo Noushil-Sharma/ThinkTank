7a4e32b94d751a2d72d6f803f3a9152d872a3570
Tamish Bajaj <100120617+TamishBajaj@users.noreply.github.com>
2024-04-24 23:33:00 +0530

    Delete team-events-backend/utils/constants.js

--- team-events-backend/utils/constants.js
+++
@@ -1,8 +0,0 @@
-secretKey="abajabadaba3456#122456"
-
-mongoURI="mongodb+srv://Tamish_minorproj:IvhrfTJpSPmGT1Oa@cluster0.m7a1xe4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-
-module.exports={
-    secretKey,
-    mongoURI
-}
